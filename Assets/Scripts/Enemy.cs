01db2997d4cb5014365bca2822c30ae20cd85201
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-10-26 02:42:53 -0700

    Unfinished
    Unfinished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,36 @@ public class Enemy : MonoBehaviour
 {
 
     public float speed = 20.0f;
+    public int standStillFrames = 30;
+    private bool isPaused = false;
+    public int HP = 2;
+    private int HPTaken =0;
+    public ParticleSystem Sparks;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (!isPaused)
+        {
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            Debug.Log("Forward");
+        }
+
+        else
+        {
+            standStillFrames--;
+
+            if (standStillFrames <= 0)
+            {
+                isPaused = false;
+            }
+        }
 
     }
     private void OnTriggerEnter(Collider other)
@@ -25,6 +45,24 @@ public class Enemy : MonoBehaviour
         Time.timeScale = 0f;
         Debug.Log("Game Over");
         }
-    }   
+
+        if (other.gameObject.CompareTag("Wall"))
+            {
+                   Vector3 newPosition = transform.position;
+                    newPosition.z = 18.2f;
+                    transform.position = newPosition;
+                    isPaused = true;
+                    Debug.Log("Pause");
+            }
+        if (other.gameObject.CompareTag("PlayerBullet"))
+        {
+            HPTaken++;
+            Sparks.Play();
+        }
+            if (HPTaken>= HP)
+            {
+                Destroy(gameObject);
+            }
+    } 
 }
 
