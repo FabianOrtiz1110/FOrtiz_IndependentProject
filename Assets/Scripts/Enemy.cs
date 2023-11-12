baa00ca57f094259b963d8bf1ccabdb024e05991
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-11-11 19:01:02 -0800

    Unfinished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,20 +11,24 @@ public class Enemy : MonoBehaviour
     public int HP = 2;
     private int HPTaken =0;
     public ParticleSystem Sparks;
+    public GameObject enemyBulletPrefab;
+    public float bulletSpeed = 10.0f;
+    public float timeBetweenShots = 12.0f;
+    public float angleInDegrees = 220f;
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(ShootCoroutine());
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (!isPaused)
         {
 
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            Debug.Log("Forward");
         }
 
         else
@@ -36,6 +40,7 @@ public class Enemy : MonoBehaviour
                 isPaused = false;
             }
         }
+        
 
     }
     private void OnTriggerEnter(Collider other)
@@ -49,20 +54,40 @@ public class Enemy : MonoBehaviour
         if (other.gameObject.CompareTag("Wall"))
             {
                    Vector3 newPosition = transform.position;
-                    newPosition.z = 18.2f;
+                    newPosition.z = 19.0f;
                     transform.position = newPosition;
                     isPaused = true;
-                    Debug.Log("Pause");
+                    Shoot();
             }
         if (other.gameObject.CompareTag("PlayerBullet"))
         {
             HPTaken++;
-            Sparks.Play();
+            Sparks.GetComponent<ParticleSystem>().Play();
+            Debug.Log("Bullet Hit");
         }
-            if (HPTaken>= HP)
-            {
-                Destroy(gameObject);
-            }
-    } 
+
+        if (HPTaken>= HP)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Shoot()
+    {      
+        for (int i = 0; i <= 280; angleInDegrees = angleInDegrees + 5 )
+        {
+        Vector3 shootDirection = new Vector3(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0f, Mathf.Sin(angleInDegrees * Mathf.Deg2Rad));
+        GameObject bullet = Instantiate(enemyBulletPrefab, transform.position, Quaternion.LookRotation(shootDirection));
+        bullet.GetComponent<Rigidbody>().velocity = shootDirection * bulletSpeed;
+        }
+    }
+     IEnumerator ShootCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeBetweenShots);
+            Shoot();
+        }
+    }
 }
 
